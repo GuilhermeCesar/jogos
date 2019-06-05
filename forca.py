f43d573214f105ee1846975fe01d2d5ef96cdcfd
guilherme <gui11_3@hotmail.com>
2019-06-04 22:21:46 -0300

    Melhorando a forca

--- forca.py
+++ forca.py
@@ -3,7 +3,17 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    # enquanto (True)
+    while not enforcou and not acertou:
+        print("Jogando ...")
+
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
